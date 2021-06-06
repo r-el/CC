ea4f028f8ff8525a6b91d3f35b3515fb180f2ee8
rel <rel@mail.com>
2021-06-06 15:39:21 +0300

    Add Food Model

---
+++ Models/Food.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaloriesCounter.Models
+{
+    public class Food
+    {
+        public Food() { }
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }        // שם מאכל
+        public string Unit { get; set; }        // יחידה
+
+        public int Calories { get; set; }       // קלוריות
+        public int Carbs { get; set; }          // פחמימות
+        public int Fat { get; set; }            // שומנים
+        public int Protein { get; set; }        // חלבונים
+        public int Sodium { get; set; }         // נתרן
+        public int Sugar { get; set; }          // סוכרים
+    }
+}
