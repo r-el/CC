bda9b068a77f744c8875d182f49eedd4b12c9a32
rel <rel@mail.com>
2021-06-06 15:20:26 +0300

    Add Taregt Model

---
+++ Models/Targets.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaloriesCounter.Models
+{
+    // יעדים
+    public class Targets
+    {
+        public Targets() { }
+
+        [Key]
+        public int ID { get; set; }
+
+        public decimal Weight { get; set; }     // משקל
+        public DateTime Date { get; set; }      // תאריך
+
+        public int Calories { get; set; }       // קלוריות
+        public int Carbs { get; set; }          // פחמימות
+        public int Fat { get; set; }            // שומנים
+        public int Protein { get; set; }        // חלבונים
+        public int Sodium { get; set; }         // נתרן
+        public int Sugar { get; set; }          // סוכרים
+    }
+}
