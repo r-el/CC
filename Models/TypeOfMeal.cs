fa958f4ff445603c49f2c854fe72ec2a5fdf6032
rel <rel@mail.com>
2021-06-06 16:05:43 +0300

    Add TypeOfMeal Model

---
+++ Models/TypeOfMeal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaloriesCounter.Models
+{
+    public class TypeOfMeal
+    {
+        // סוג הארוחה 
+        public TypeOfMeal() { }
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; } // שם הארוחה
+    }
+}
