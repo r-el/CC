7a7b103b398d5bb98e067767bc4e975098b1ede7
rel <rel@mail.com>
2021-06-09 14:23:22 +0300

    Targets reference and Add Date, AddFood/s in Meal Model

--- Models/Meal.cs
+++ Models/Meal.cs
@@ -12,8 +12,28 @@ namespace CaloriesCounter.Models
         public Meal() { Foods = new List<FoodsInMeal>(); }
         [Key]
         public int ID { get; set; }
-        public User User { get; set; }                 // משתמש
+        [Display(Name = "תאריך")]
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }             // תאריך
         public TypeOfMeal TypeOfMeal { get; set; }     // סוג הארוחה
         public List<FoodsInMeal> Foods { get; set; }   // מאכלים בארוחה
+        public Targets Target { get; set; }            // שיוך הארוחה ליעדים
+
+        // הוספת כמה מאכלים לארוחה
+        public void AddFoods(List<Food> foods)
+        {
+            foreach (Food food in foods)
+            {
+                AddFood(food, 1); // הכנס כל מאכל לרשימה עם כמות של 100 גרם לכל מאכל
+            }
+        }
+        // הוספת מאכל יחיד לארוחה
+        public void AddFood(Food food, double? quantity )
+        {
+            double myQuantity = 1;
+            if (quantity != null)
+                myQuantity = quantity.Value;
+            Foods.Add(new FoodsInMeal() { Food=food, Meal=this, Quantity=myQuantity  } );
+        }
     }
 }
