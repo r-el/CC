bd6ac8f596b9f66da332122942a8bde957f27877
rel <rel@mail.com>
2021-06-09 15:18:15 +0300

    Add AddTargets(DateTime dateOfFirstTargets, List<Targets> targets((each 7 days))), AddTargets(List<Targets> targets), AddTarget(Targets target) in Plans Model

--- Models/Plans.cs
+++ Models/Plans.cs
@@ -16,20 +16,29 @@ namespace c_c.Models
         public DateTime Date { get; set; }          // תאריך
         public List<Targets> Targets { get; set; }  // יעדים
 
-        // הוספת כמה תוכניות
-        public void AddTargets(DateTime Date, List<Targets> targets)
+        // הוספת מספר תוכניות
+        public void AddTargets(DateTime dateOfFirstTargets, List<Targets> targets)
         {
-
-
+            // הכנסת היעדים כשבוע
+            foreach (Targets target in targets)
+            {
+                target.Date = dateOfFirstTargets;
+                dateOfFirstTargets = dateOfFirstTargets.AddDays(7);
+            }
+            AddTargets(targets);
         }
         public void AddTargets(List<Targets> targets)
         {
-
+            foreach (Targets target in targets)
+            {
+                AddTarget(target);
+            }
         }
         // הוספת תוכנית אחת
-        public void AddTarget(Targets Target)
+        public void AddTarget(Targets target)
         {
-
+            Targets.Add(target);
+            target.Plan = this;
         }
     }
 }
