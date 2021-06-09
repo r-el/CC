4c2a527fc26c016a1408f99bd59dcfa38123d832
rel <rel@mail.com>
2021-06-09 20:58:49 +0300

    Add Connect.js

---
+++ wwwroot/js/Connect.js
@@ -0,0 +1,8 @@
+﻿function ShowNewUser() {
+    document.getElementById("connect").style.display = "none";
+    document.getElementById("NewUser").style.display = "";
+}
+function ShowConnect() {
+    document.getElementById("connect").style.display = "";
+    document.getElementById("NewUser").style.display = "none";
+}
