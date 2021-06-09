681530b3d588950235afea2074f27b4fe3bf5bae
rel <rel@mail.com>
2021-06-09 21:12:55 +0300

    Add VMNewMeal

---
+++ ViewModel/VMNewMeal.cs
@@ -0,0 +1,15 @@
+﻿using CaloriesCounter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaloriesCounter.ViewModel
+{
+    public class VMNewMeal
+    {
+        public VMNewMeal() { }
+        public Meal Meal { get; set; }
+        public int TypeID { get; set; }
+    }
+}
