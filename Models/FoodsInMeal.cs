b4f62ede0d4a567a66f0a9b047747f600d5110ee
rel <rel@mail.com>
2021-06-09 16:03:29 +0300

    Add TypeOfMeal in FoodsInMeal Model

--- Models/FoodsInMeal.cs
+++ Models/FoodsInMeal.cs
@@ -12,9 +12,11 @@ namespace CaloriesCounter.Models
         public FoodsInMeal() { }
         [Key]
         public int ID { get; set; }
-        public Meal Meal { get; set; }      // שיוך לארוחה
-        public Food Food { get; set; }      // שיוך למאכל
         [Display(Name = "כמות")]
-        public double Quantity { get; set; }   // כמות בגרם
+        public double Quantity { get; set; }            // כמות בגרם
+        public TypeOfMeal TypeOfMeal { get; set; }      // סוג ארוחה
+        public Meal Meal { get; set; }                  // שיוך לארוחה
+        public Food Food { get; set; }                  // שיוך למאכל
+        /* -------להוסיף מצב רוח ופעילות-------- */
     }
 }
