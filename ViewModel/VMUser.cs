7df0fa5abf74dac3f1d05002958ecfcd6f1c4f8c
rel <rel@mail.com>
2021-06-09 16:21:13 +0300

    Add VMUser

---
+++ ViewModel/VMUser.cs
@@ -0,0 +1,19 @@
+﻿using CaloriesCounter.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaloriesCounter.ViewModel
+{
+    // כדי לאפשר למשתמש להכניס משקל בפעם הראשונה
+    public class VMUser
+    {
+        public VMUser() { User = new User(); }
+
+        public User User { get; set; }          // משתמש
+        [Display(Name = "משקל")]
+        public decimal Weight { get; set; }     // משקלים
+    }
+}
