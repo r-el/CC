ef1e0e57e0b9110a136a0d6f244f43041582bd64
rel <rel@mail.com>
2021-06-09 21:15:06 +0300

    Add VMNewPlan

---
+++ ViewModel/VMNewPlan.cs
@@ -0,0 +1,26 @@
+﻿using CaloriesCounter.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CaloriesCounter.ViewModel
+{
+    public class VMNewPlan
+    {
+        // אתחול ארוחות
+        public VMNewPlan() { Meals = new List<VMNewMeal>(); }
+
+        public User User { get; set; }                   // נתוני משתמש
+
+        public int UserID { get; set; }                  // שיוך של התוכנית ליוזר כשהוא חוזר מהוויו
+
+        public List<Food> Foods { get; set; }            // מאכלים
+
+        public List<TypeOfMeal> Types { get; set; }      // סוגי ארוחות
+
+        public List<VMNewMeal> Meals { get; set; }       // ליסט ארוחות
+
+        public VMNewMeal Meal { get; set; }              // ארוחה
+    }
+}
